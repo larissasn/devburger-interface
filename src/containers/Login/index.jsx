ee0038617d78597d1f91e6427fb00f89ed0aeae5
Larissa Santos <106403123+larissasn@users.noreply.github.com>
2024-09-17 20:44:46 -0300

    feat: Iniciado a criação da tela de Login.

--- src/containers/Login/index.jsx
+++ src/containers/Login/index.jsx
@@ -1,7 +1,33 @@
+import { Button, Container, Form, InputContainer, LeftContainer, Link, RightContainer, Title } from './styles';
+import Logo from '../../assets/logo.svg';
+
 export function Login() {
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <Container>
+      <LeftContainer>
+        <img src={Logo} alt="logo-devburger" />
+      </LeftContainer>
+      <RightContainer>
+        <Title>
+          Olá, seja bem vindo ao <span>Dev Burguer!</span>Dev Burguer! Acesse
+          com seu <span>Login e senha.</span>
+        </Title>
+        <Form>
+          <InputContainer>
+            <label>Email</label>
+            <input type="emial" />
+          </InputContainer>
+
+          <InputContainer>
+            <label>Senha</label>
+            <input type="password" />
+          </InputContainer>
+          <Link>Esqueci minha senha.</Link>
+          <Button>Entrar</Button>
+        </Form>
+        <Link> Não possui conta? Clique aqui.</Link>
+      </RightContainer>
+      :
+    </Container>
   );
 }
